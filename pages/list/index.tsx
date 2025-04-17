de7445040b5326ab6b647c6f9ac269260e825922
Marie Berezhna <marie.berezhna@gmail.com>
2025-04-17 17:48:54 +0200

    page headings

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -7,7 +7,7 @@ export default function ListPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>List</h1>
+          <h1 className={title()}>Memes List</h1>
         </div>
         <MemesList />
       </section>
--- pages/table/index.tsx
+++ pages/table/index.tsx
@@ -7,7 +7,7 @@ export default function TablePage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Table</h1>
+          <h1 className={title()}>Memes Table</h1>
         </div>
         <MemeTable />
       </section>
